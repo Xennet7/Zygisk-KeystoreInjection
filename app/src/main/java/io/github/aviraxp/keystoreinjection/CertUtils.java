f7fb854069f47aee19536f2194173a5c2130c0d4
XENNET <xennet@rediffmail.com>
2025-10-18 21:19:16 +0530

    Update CertUtils.java

--- app/src/main/java/io/github/aviraxp/keystoreinjection/CertUtils.java
+++ app/src/main/java/io/github/aviraxp/keystoreinjection/CertUtils.java
@@ -3,9 +3,12 @@ package io.github.aviraxp.keystoreinjection;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.util.io.pem.PemObject;
 import org.bouncycastle.util.io.pem.PemReader;
 
@@ -14,6 +17,7 @@ import java.io.StringReader;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
 import java.util.regex.Pattern;
 
 public class CertUtils {
@@ -25,9 +29,7 @@ public class CertUtils {
         try (PemReader reader = new PemReader(new StringReader(cert))) {
             pemObject = reader.readPemObject();
         }
-
         X509CertificateHolder holder = new X509CertificateHolder(pemObject.getContent());
-
         return (new JcaX509CertificateConverter().getCertificate(holder));
     }
 
@@ -37,9 +39,7 @@ public class CertUtils {
         try (PemReader reader = new PemReader(new StringReader(cert))) {
             pemObject = reader.readPemObject();
         }
-
         X509CertificateHolder holder = new X509CertificateHolder(pemObject.getContent());
-
         return holder.getSubject();
     }
 
@@ -49,9 +49,7 @@ public class CertUtils {
         try (PEMParser parser = new PEMParser(new StringReader(key))) {
             object = parser.readObject();
         }
-
         PEMKeyPair pemKeyPair = (PEMKeyPair) object;
-
         return new JcaPEMKeyConverter().getKeyPair(pemKeyPair);
     }
 
@@ -63,5 +61,23 @@ public class CertUtils {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // --- NEW: Dummy certificate for TEE-broken devices ---
+    public static X509Certificate buildDummyCert(KeyPair kp, String subjectCN) throws Exception {
+        X500Name subject = new X500Name(subjectCN);
+        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
+                subject,
+                java.math.BigInteger.valueOf(System.currentTimeMillis()),
+                new java.util.Date(System.currentTimeMillis() - 1000L * 60 * 60),
+                new java.util.Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 365),
+                subject,
+                kp.getPublic()
+        );
+
+        String algo = kp.getPublic().getAlgorithm().equals("EC") ? "SHA256withECDSA" : "SHA256withRSA";
+        ContentSigner signer = new JcaContentSignerBuilder(algo).build(kp.getPrivate());
+
+        return new JcaX509CertificateConverter().getCertificate(certBuilder.build(signer));
     }
 }
